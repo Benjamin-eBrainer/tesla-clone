53c77df680b40fa0477cf1d01e4fd3a191b9639d
Benjamin-eBrainer <515ebrainer@gmail.com>
2023-07-11 12:43:20 +0300

    Added redux

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import { Provider } from 'react-redux';
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
+import { store } from './backend/store';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Provider store={store}>
+        <Header />
+      </Provider>
       <Home />
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -8,9 +8,12 @@ import {
   CloseWrapper,
   CustomClose
 } from '../styled components/headerStyles'
+import { selectCars } from '../backend/cars/carSlice';
+import { useSelector } from 'react-redux';
 
 function Header() {
   const [burgerStatus, setBurgerStatus] = useState(false);
+  const cars = useSelector(selectCars);
 
   return (
     <Container>
@@ -18,10 +21,11 @@ function Header() {
         <img src="/images/logo.svg" alt="Tesla" />
       </a>
       <Menu>
-        <a href="#">Model S</a>
-        <a href="#">Model 3</a>
-        <a href="#">Model X</a>
-        <a href="#">Model Y</a>
+        {
+          cars && cars.map((car, index) => (
+            <a key={index} href="#">{car}</a>
+          ))
+        }
       </Menu>
       <RightMenu>
         <a href="#">Shop</a>
@@ -32,14 +36,16 @@ function Header() {
         <CloseWrapper>
           <CustomClose onClick={() => setBurgerStatus(false)} />
         </CloseWrapper>
+        {
+          cars && cars.map((car, index) => (
+            <li key={index}><a href="#">{car}</a></li>
+          ))
+        }
         <li><a href="#">Existing Inventory</a></li>
         <li><a href="#">Used Inventory</a></li>
         <li><a href="#">Trade-in</a></li>
         <li><a href="#">Cybertruck</a></li>
         <li><a href="#">Roadaster</a></li>
-        <li><a href="#">Existing Inventory</a></li>
-        <li><a href="#">Existing Inventory</a></li>
-        <li><a href="#">Existing Inventory</a></li>
       </BurgerNav>
     </Container>
   )
